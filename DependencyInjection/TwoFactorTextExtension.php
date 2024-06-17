f6d5e41e7e803d17f325817c53b632f9db8d1ca4
Michael Erkens <10248696+erkens@users.noreply.github.com>
2024-06-17 22:06:52 +0200

    Fix #10 Symfony deprecation for DependencyInjection (#11)

--- DependencyInjection/TwoFactorTextExtension.php
+++ DependencyInjection/TwoFactorTextExtension.php
@@ -6,9 +6,9 @@ namespace Erkens\Security\TwoFactorTextBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class TwoFactorTextExtension extends Extension implements PrependExtensionInterface
 {
